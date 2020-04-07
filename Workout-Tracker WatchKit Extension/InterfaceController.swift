fbab82ac9b72fd286934a212ced7a9b96e5f2158
Donald Seo <deokjae.seo@gmail.com>
2020-04-06 20:28:37 -0400

    Initial Commit

---
+++ Workout-Tracker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Workout-Tracker WatchKit Extension
+//
+//  Created by Donald Seo on 2020-04-06.
+//  Copyright © 2020 Donald Seo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
